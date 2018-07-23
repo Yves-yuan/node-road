bb32df8b3991bd93bb7d191d671eb7c11f44282b
yuanyifei <yuanp0813@163.com>
2018-07-23 17:31:02 +0800

    修改ui

--- routes/road.js
+++ routes/road.js
@@ -3,15 +3,19 @@ var fs = require('fs');
 let router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    let s = req.query.start;
-    let e = req.query.end;
-    fs.readFile('data/drivingdatarealpos.txt','utf-8',function(err,data){
+    let s = parseInt(req.query.start);
+    let e = parseInt(req.query.end);
+    if (s > e) {
+        res.send({err:"起始索引不能比结束索引大"});
+        return;
+    }
+    fs.readFile('data/drivingdatarealpos.txt', 'utf-8', function (err, data) {
         let result;
         if (err) {
             res.error(err)
         } else {
             let lines = data.split("\n");
-            fs.readFile('data/Results.txt','utf-8',function(err,data1) {
+            fs.readFile('data/Results.txt', 'utf-8', function (err, data1) {
                 if (err) {
                     res.error(err);
                     return;
@@ -19,7 +23,7 @@ router.get('/', function (req, res, next) {
                 let linesCategory = data1.split("\n");
                 result = [];
                 for (let i = s; i < e; i++) {
-                    result.push({pos:lines[i],category:linesCategory[i]});
+                    result.push({pos: lines[i], category: linesCategory[i]});
                 }
                 let j = JSON.stringify(result);
                 res.send(j);
